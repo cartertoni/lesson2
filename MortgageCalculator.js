7c6c734b7feb0d39d82d02c634be3daff0edda22
Carter Toni <carter.blaine.toni@gmail.com>
2024-05-27 20:38:27 -0700

    Refactor calculator and extract messages to json

--- MortgageCalculator.js
+++ MortgageCalculator.js
@@ -1,32 +1,106 @@
-const rlSync = require('readline-sync');
+let messages = require('./MortgageCalculator_Messages.json');
+let rlSync = require('readline-sync');
 
-const prompt = message => rlSync.question(`${message} \n`);
+let prompt = message => {
+  return rlSync.question(`=> ${message} \n`);
+};
 
-let loanAmount = prompt('What is the loan amount?');
-let annualPercentageRate = parseFloat(
-  prompt('What is the Annual Percentage Rate of this loan? Example: 5.85%')
-);
-let monthlyPercentageRate = annualPercentageRate / 100 / 12;
+let invalidChoice = input => console.log(`Please enter a valid ${input}`);
 
-let loanYears = parseInt(prompt('How many years remain on this loan?'), 10);
+let getLoanAmount = () => {
+  let loanAmount;
+  do {
+    if (loanAmount !== undefined) {
+      invalidChoice('loan amount');
+    }
+    loanAmount = parseFloat(prompt(messages.loanAmount).replace(/,/g, ''), 10);
+  } while (!(loanAmount > 0));
+  return loanAmount;
+};
 
-let loanMonths = parseInt(prompt('How many months remain on this loan?'), 10);
+let getAnnualPercentageRate = () => {
+  let annualPercentageRate;
+  do {
+    if (annualPercentageRate !== undefined) {
+      invalidChoice('annual percentage rate');
+    }
+    annualPercentageRate = parseFloat(prompt(messages.annualPercentageRate));
+  } while (!(annualPercentageRate >= 0));
+  return annualPercentageRate;
+};
 
-let lengthOfLoan = loanYears * 12 + loanMonths;
+let getLoanLength = () => {
+  let loanMonths, loanYears;
+  do {
+    if (loanYears !== undefined) {
+      invalidChoice('number of years');
+    }
+    loanYears = parseInt(prompt(messages.loanYears), 10);
+  } while (!(loanYears >= 0));
 
-let monthlyPayment;
+  do {
+    if (loanMonths !== undefined) {
+      invalidChoice('number of months');
+    }
+    loanMonths = parseInt(prompt(messages.loanMonths), 10);
+  } while (!(loanMonths >= 0 && loanMonths <= 11));
 
-if (monthlyPercentageRate !== 0) {
-  monthlyPayment =
-    loanAmount *
-    (monthlyPercentageRate /
-      (1 - Math.pow(1 + monthlyPercentageRate, -lengthOfLoan)));
-} else {
-  monthlyPayment = loanAmount / lengthOfLoan;
-}
+  return { years: loanYears, months: loanMonths };
+};
 
-console.log(
-  `You must make ${lengthOfLoan} monthly payments of $${monthlyPayment.toFixed(
-    2
-  )}`
-);
+let calculateLoan = (amount, annualPercentageRate, loanYears, loanMonths) => {
+  let monthlyPercentageRate = annualPercentageRate / 100 / 12;
+  let lengthOfLoan = loanYears * 12 + loanMonths;
+  let monthlyPayment;
+
+  if (!lengthOfLoan) return console.log(`You must immediately pay $${amount}.`);
+
+  if (monthlyPercentageRate !== 0) {
+    monthlyPayment =
+      amount *
+      (monthlyPercentageRate /
+        (1 - Math.pow(1 + monthlyPercentageRate, -lengthOfLoan)));
+  } else {
+    monthlyPayment = amount / lengthOfLoan;
+  }
+  console.clear();
+  return console.log(
+    `To repay the $${amount} loan at ${annualPercentageRate}%, you must make ${lengthOfLoan} monthly ${
+      lengthOfLoan === 1 ? 'payment' : 'payments'
+    } of $${monthlyPayment.toFixed(2)}`
+  );
+};
+
+let calculateAgain = () => {
+  let userInput;
+  do {
+    if (userInput !== undefined) {
+      invalidChoice('choice: Yes/No');
+    }
+    userInput = prompt('Would you like to do another loan calculation? Yes/No')
+      .toLowerCase()
+      .trim();
+  } while (!(userInput === 'yes' || userInput === 'no'));
+  if (userInput === 'yes') return true;
+  else return false;
+};
+
+let runCalculator = () => {
+  console.clear();
+  console.log(messages.welcome);
+  let loanAmount = getLoanAmount();
+  let annualPercentageRate = getAnnualPercentageRate();
+  let loanLength = getLoanLength();
+  return calculateLoan(
+    loanAmount,
+    annualPercentageRate,
+    loanLength.years,
+    loanLength.months
+  );
+};
+
+do {
+  runCalculator();
+} while (calculateAgain());
+
+console.log(messages.goodbye);
